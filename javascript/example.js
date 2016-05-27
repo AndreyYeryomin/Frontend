a381e49dbf08aef5687e3a90c061d54b6072cf01
AndreyYeryomin <andrey.yeryomin11@gmail.com>
2016-05-27 20:46:18 +0300

    weekmap

--- javascript/example.js
+++ javascript/example.js
@@ -1,5 +1,27 @@
-name = prompt("Item:");
-price= +prompt("Price");
-count= +prompt("Count");
-alert('Thanks you' +name+ 'Total:'+price*count);
+var weekMap_obj ={
+    1: "Monday",
+    2:  "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday"
+};
+var weekMap_arr =[
+     "Monday",
+    "Tuesday",
+   "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+];
 
+var input= +prompt("enter"),
+    result_obj = weekMap_obj[input],
+    result_arr = weekMap_arr[input-1];
+if(!result_obj){
+    result_obj= "Surprise!"
+}
+console.log(result_obj);
+console.log(result_arr);
